b6faf03da095bc71b60b259876bcd2de2e070bba
panjiajie <251907882@qq.com>
2021-01-19 20:23:15 +0800

    master->升级webpack和webpack-cli至4.0版本->panjiajie

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,24 +3,24 @@
 //会根据本文件的配置详情进行打包
 
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const Webpack = require('webpack')
-const NpmInstallWebpackPlugin = require('npm-install-webpack-plugin') //自动npm -i缺少的npm 包
-const CompressionPlugin = require("compression-webpack-plugin") //压缩代码
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Webpack = require('webpack');
+const NpmInstallWebpackPlugin = require('npm-install-webpack-plugin'); //自动npm -i缺少的npm 包
+const CompressionPlugin = require("compression-webpack-plugin"); //压缩代码
 module.exports = {
     entry: function (){ //入口文件
-        return path.join(__dirname,"../src/index.js")
+        return path.join(__dirname,"../src/index.js");
     },
-    output:{
-        path:path.join(__dirname,"../build"),
-        filename:'index.js'
+    output: {
+        path: path.join(__dirname,"../build"),
+        filename: 'index.js'
     },
-    module:{
-        rules:[
-            { test: /\.less$/,use:['css-loader','style-loader',"less-loader"] }, //预处理less
-            { test: /\.css$/, use:['css-loader','style-loader'] },//预处理css
-            { test: /\.(png|jpg|jpeg|gif)$/, use:['file-loader']},
-            { test:/\.js$/,use:[  'babel-loader' ],exclude: /(node_modules|bower_components)/ }
+    module: {
+        rules: [
+            { test: /\.less$/,use: ['css-loader','style-loader','less-loader'] }, //预处理less
+            { test: /\.css$/, use: ['css-loader','style-loader'] },//预处理css
+            { test: /\.(png|jpg|jpeg|gif)$/, use: ['file-loader']},
+            { test: /\.js$/,use: ['babel-loader'],exclude: /(node_modules|bower_components)/ }
         ]
     },
     plugins: [
@@ -28,11 +28,11 @@ module.exports = {
         //     threshold:1024*1
         // }),
         new HtmlWebpackPlugin({
-            template:path.join(__dirname,"../public/index.html"),
-            inject:true,
-            filename:"index.html"
+            template: path.join(__dirname,"../public/index.html"),
+            inject: true,
+            filename: "index.html"
         }),
         new Webpack.HotModuleReplacementPlugin(),
         new NpmInstallWebpackPlugin() //在开发时候自动安装缺少的依赖
     ]
-}
+};
